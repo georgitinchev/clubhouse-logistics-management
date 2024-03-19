7d319961a492861636e59685752932a5c68d9946
georgitinchev <g.tinchev@student.fontys.nl>
2024-03-19 14:21:04 +0100

    Better repo folder structure + Dashboard form obj initialization

--- MediaBazaar/MediaBazaar/Forms/Dashboard.cs
+++ MediaBazaar/MediaBazaar/Forms/Dashboard.cs
@@ -1,12 +1,20 @@
+using MediaBazaar.Classes;
+
 namespace MediaBazaar
 {
 
 	public partial class Dashboard : Form
 	{
+		private EmployeeManager employeeManager;
+		private DatabaseOperations dbOperations;
+		private List<Employee> employees;
+
 		public Dashboard()
 		{
 			Application.EnableVisualStyles();
 			InitializeComponent();
+			dbOperations = new DatabaseOperations();
+			employeeManager = new EmployeeManager();
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
