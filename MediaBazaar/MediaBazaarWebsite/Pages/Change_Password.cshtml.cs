97c3575e0a832c42178e32dea1bfd57130014276
Matei Copoeru <m.copoeru@student.fontys.nl>
2024-04-19 04:38:04 +0200

    Fixed password reset and delete employee in desktop app

---
+++ MediaBazaar/MediaBazaarWebsite/Pages/Change_Password.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MediaBazaarWebsite.Pages
+{
+    public class Change_PasswordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
