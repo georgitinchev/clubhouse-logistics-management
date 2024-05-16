3b7cb5066bf813bb668d870526d7c80439068c61
georgitinchev <g.tinchev@student.fontys.nl>
2024-05-16 22:07:32 +0200

    Mainform improvements for depot mgmt part

--- MediaBazaar/MediaBazaar/Forms/MainForm.cs
+++ MediaBazaar/MediaBazaar/Forms/MainForm.cs
@@ -44,6 +44,7 @@ namespace MediaBazaar
             logOutBtn.Location = new Point(panel1.Width - logOutBtn.Width - 25, 9);
             pictureBox1.Location = new Point(btnDashboard.Location.X + (btnDashboard.Width - pictureBox1.Width) / 2, btnDashboard.Location.Y + btnDashboard.Height + 10);
             lineEmployee.Location = new Point(btnWorksheet.Location.X + (btnWorksheet.Width - lineEmployee.Width) / 2, btnWorksheet.Location.Y + btnWorksheet.Height + 10);
+            lineProduct.Location = new Point(btnProduct.Location.X + (btnProduct.Width - lineProduct.Width) / 2, btnProduct.Location.Y + btnProduct.Height + 10);
             userControlDashboard.Size = new Size(this.ClientSize.Width, this.ClientSize.Height - panel1.Height);
             userControlDashboard.Location = new Point(0, panel1.Height);
             userControlWorksheet.Size = new Size(this.ClientSize.Width, this.ClientSize.Height - panel1.Height);
@@ -55,6 +56,7 @@ namespace MediaBazaar
         private void btnDashboard_Click(object sender, EventArgs e)
         {
             userControlWorksheet.Hide();
+            userControlDeport.Hide();
             userControlDashboard.InitializeData();
             userControlDashboard.Show();
         }
@@ -62,8 +64,18 @@ namespace MediaBazaar
         private void btnWorksheet_Click(object sender, EventArgs e)
         {
             userControlDashboard.Hide();
+            userControlDeport.Hide();
             userControlWorksheet.PopulateWorksheetData();
             userControlWorksheet.Show();
+        }
+
+        private void btnProduct_Click(object sender, EventArgs e)
+        {
+            userControlDashboard.Hide();
+            userControlWorksheet.Hide();
+            userControlDeport.InitializeProductData();
+            userControlDeport.ClearProductDetails();
+            userControlDeport.Show();
         }
 
         private void logOutBtn_Click(object sender, EventArgs e)
@@ -79,13 +91,6 @@ namespace MediaBazaar
             {
                 this.Close();
             }
-        }
-
-        private void btnProduct_Click(object sender, EventArgs e)
-        {
-            userControlDashboard.Hide();
-            userControlWorksheet.Hide();
-            userControlDeport.Show();
         }
     }
 }
