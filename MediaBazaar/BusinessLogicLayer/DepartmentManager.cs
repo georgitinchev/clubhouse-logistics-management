c896b7917cebf905d8e4de43385258fff49201e1
Matei Copoeru <m.copoeru@student.fontys.nl>
2024-06-24 21:15:16 +0200

    Added Department Manager and stuff

---
+++ MediaBazaar/BusinessLogicLayer/DepartmentManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class DepartmentManager : IDepartmentManager
+    {
+        private readonly List<Department> _departments = new List<Department>();
+
+        public void Add(Department department)
+        {
+            _departments.Add(department);
+        }
+
+        public void Update(Department department)
+        {
+            var existing = _departments.FirstOrDefault(d => d.Id == department.Id);
+            if (existing != null)
+            {
+                existing.Name = department.Name;
+                existing.Description = department.Description;
+                existing.RequiredPersonnel = department.RequiredPersonnel;
+                existing.PointOfContact = department.PointOfContact;
+            }
+        }
+
+        public void Remove(int id)
+        {
+            var department = _departments.FirstOrDefault(d => d.Id == id);
+            if (department != null)
+            {
+                _departments.Remove(department);
+            }
+        }
+
+        public Department GetById(int id)
+        {
+            return _departments.FirstOrDefault(d => d.Id == id);
+        }
+
+        public IEnumerable<Department> Search(string query)
+        {
+            return _departments.Where(d => d.Name.Contains(query) || d.Description.Contains(query));
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _departments;
+        }
+    }
+}
---
+++ MediaBazaar/BusinessLogicLayer/IDepartmentManager.cs
@@ -0,0 +1,13 @@
+﻿
+namespace BusinessLogicLayer
+{
+    public interface IDepartmentManager
+    {
+        void Add(Department department);
+        IEnumerable<Department> GetAll();
+        Department GetById(int id);
+        void Remove(int id);
+        IEnumerable<Department> Search(string query);
+        void Update(Department department);
+    }
+}
