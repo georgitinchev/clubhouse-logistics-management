6c8160cefc7cfbad59637743bf4815bfe3fc75af
georgitinchev <g.tinchev@student.fontys.nl>
2024-06-27 10:49:40 +0200

    Added metrics to product mgmt form

---
+++ MediaBazaar/BusinessLogicLayer/Interfaces/IProductManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Interfaces
+{
+	public interface IProductManager
+	{
+		List<Product> GetAllProducts();
+		Product GetProductById(int id);
+		void AddProduct(Product product);
+		void UpdateProduct(Product product);
+		void DeleteProduct(int id);
+	}
+}
